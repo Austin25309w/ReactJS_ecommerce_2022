1888cd7ca702107a52a983003238ac8a52cf7105
Austin <austinow@gmail.com>
2021-12-15 17:07:56 -0800

    make sure added everything

---
+++ src/Lifecycles.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Lifecycles extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            
+        }
+    }
+
+    render(){
+        return <div>Lifecycle</div>
+    }
+}
+
+export default Lifecycles
